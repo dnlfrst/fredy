126ce9462b54a78bdc6f54788f45371d6e25ebbe
Daniel Fürst <dpfuerst@gmail.com>
2024-10-13 20:53:04 +0200

    User provider’s name instead of ID for notification adapters

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ setInterval(
             .forEach(async (prov) => {
               const pro = fetchedProvider.find((fp) => fp.metaInformation.id === prov.id);
               pro.init(prov, job.blacklist);
-              await new FredyRuntime(pro.config, job.notificationAdapter, prov.id, job.id, similarityCache).execute();
+              await new FredyRuntime(pro.config, job.notificationAdapter, prov.id, prov.name, job.id, similarityCache).execute();
               setLastJobExecution(job.id);
             });
         });
